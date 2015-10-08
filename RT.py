465047c7cbfa30be77cbc6e2fd90f8a55eab73ab
unknown <jaclyncnguyen@utexas.edu>
2015-10-08 15:06:22 -0700

    updated files to correct new website

--- RT.py
+++ RT.py
@@ -23,7 +23,7 @@ def write_allearnings(weekend, rank, rating2, name, weeks, wkend_gross):
 def latest_earnings():
     rotten = feedparser.parse('http://www.rottentomatoes.com/browse/box-office')
 
-    movie_urls = set()
+    movie_names = set()
     soup = BeautifulSoup(rotten['feed']['summary'])
 
     links = soup.find('form', {'class': 'rt fl'})
@@ -46,7 +46,7 @@ def latest_earnings():
             continue
         name = earnings[i +3].find('a',{'target':'_top'}).contents[0]
         movie_url = earnings[i+3].a['href']
-        movie_urls.add(movie_url)
+        movie_names.add(name)
         weeks = (earnings[i+4]).contents[0]
         wkend_gross = (earnings[i+5]).contents[0]
         total_gross = (earnings[i+6]).contents[0]
@@ -54,9 +54,9 @@ def latest_earnings():
         num_theat = (earnings[i+8]).contents[0]
 
         write_output(rank, last_rank, rating2, name, weeks, wkend_gross, total_gross, theat_avg, num_theat)
-    return movie_urls, date_links
+    return movie_names, date_links
 
-def gross_earning(movie_urls, date_links):
+def gross_earning(movie_names, date_links):
     for link in date_links:
         new_url = date_links[link]
         rotten2 = feedparser.parse(new_url)
@@ -74,32 +74,16 @@ def gross_earning(movie_urls, date_links):
             try:
                 name = gross_earnings[i +2].find('a',{'target':'_top'}).contents[0]
                 movie_url = gross_earnings[i+2].a['href']
-                movie_urls.add(movie_url)
+                movie_names.add(name)
             except:
                 continue
             wkend_gross = (gross_earnings[i+4]).contents[0]
             weeks = (gross_earnings[i+3]).contents[0]
             write_allearnings(link, rank, rating2, name, weeks, wkend_gross)
-    return movie_urls
+    return movie_names
 
 def main():
-    movie_urls, date_links = latest_earnings()
-    movie_urls2 = gross_earning(movie_urls, date_links)
-
+    movie_names, date_links = latest_earnings()
+    movie_names2 = gross_earning(movie_names, date_links)
+    print movie_names2
 main()
-# <form class="rt fl">
-#  <option value="/browse/box-office/?rank_id=0&country=us">Last Weekend</option>
-# <option value="/browse/box-office/?rank_id=1&country=us">Sep 18-Sep 20</option>
-# <option value="/browse/box-office/?rank_id=2&country=us">Sep 11-Sep 13</option>
-# <option value="/browse/box-office/?rank_id=3&country=us">Sep 04-Sep 06</option>
-# <option value="/browse/box-office/?rank_id=4&country=us">Aug 28-Aug 30</option>
-# <option value="/browse/box-office/?rank_id=5&country=us">Aug 21-Aug 23</option>
-# <option value="/browse/box-office/?rank_id=6&country=us">Aug 14-Aug 16</option>
-# <option value="/browse/box-office/?rank_id=7&country=us">Aug 07-Aug 09</option>
-# <option value="/browse/box-office/?rank_id=8&country=us">Jul 31-Aug 02</option>
-# <option value="/browse/box-office/?rank_id=9&country=us">Jul 24-Jul 26</option>
-# <option value="/browse/box-office/?rank_id=10&country=us">Jul 17-Jul 19</option>
-# <option value="/browse/box-office/?rank_id=11&country=us">Jul 10-Jul 12</option>
-# <option value="/browse/box-office/?rank_id=12&country=us">Jul 03-Jul 05</option>
-# <option value="/browse/box-office/?rank_id=13&country=us">Jun 26-Jun 28</option>
-# <option value="/browse/box-office/?rank_id=14&country=us">Jun 19-Jun 21</option>
